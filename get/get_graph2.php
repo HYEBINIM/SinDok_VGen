60931182a3b9ed25de9badbe637f92047c833a6c
임혜빈 <ihb3470@kakao.com>
2024-05-22 16:48:28 +0900

    240522_메모리부족해결
    기존 : graph.html
    변경 : test.html
    기존 파일의 메모리가 기하급수적으로 사용량이 증가하여 메모리 부족 현상을 해결하기 위해 그래프를 파괴하고 생성하는 방법이 아닌 Chart.js의 UPDATE API를 사용함

---
+++ get/get_graph2.php
@@ -0,0 +1,44 @@
+<?php
+session_start();
+$conn = mysqli_connect("localhost", "server", "00000000", "dataset");
+
+$array1 = array();
+$array2 = array();
+$array3 = array();
+$array4 = array();
+$array5 = array();
+$array6 = array();
+
+$sql = "select * from tracking02 order by id desc limit 50";
+$res = mysqli_query($conn, $sql);
+
+for(; $row = mysqli_fetch_array($res);){
+
+    if($row['data0'] == null){$item0 = 0;}else{$item0 = $row['data0'];}
+    if($row['data1'] == null){$item1 = 0;}else{$item1 = $row['data1'];}
+    if($row['data2'] == null){$item2 = 0;}else{$item2 = $row['data2'];}
+    if($row['data3'] == null){$item3 = 0;}else{$item3 = $row['data3'];}
+    if($row['data4'] == null){$item4 = 0;}else{$item4 = $row['data4'];}
+    if($row['data5'] == null){$item5 = 0;}else{$item5 = $row['data5'];}
+
+    array_push($array1, (int)$item0);
+    array_push($array2, (int)$item1);
+    array_push($array3, (int)$item2);
+    array_push($array4, (int)$item3);
+    array_push($array5, (int)$item4);
+    array_push($array6, (int)$item5);
+
+}
+
+$data = array(
+    $array1,
+    $array2,
+    $array3,
+    $array4,
+    $array5,
+    $array6
+);
+
+echo json_encode($data);
+
+?>
---
+++ get/get_graph4.php
@@ -0,0 +1,29 @@
+<?php
+session_start();
+$conn = mysqli_connect("localhost", "server", "00000000", "dataset");
+
+$array1 = array();
+$array2 = array();
+
+
+$sql = "select * from tracking04 order by id desc limit 50";
+$res = mysqli_query($conn, $sql);
+
+for(; $row = mysqli_fetch_array($res);){
+
+    if($row['data0'] == null){$item0 = 0;}else{$item0 = $row['data0'];}
+    if($row['data1'] == null){$item1 = 0;}else{$item1 = $row['data1'];}
+
+    array_push($array1, (int)$item0);
+    array_push($array2, (int)$item1);
+
+}
+
+$data = array(
+    $array1,
+    $array2
+);
+
+echo json_encode($data);
+
+?>
